a857e7ab67e1d0ddcd8263d1e50a3711983e455c
João Vitor Simonetti <jvsimonetti@outlook.com>
2026-01-15 15:15:09 -0400

    feat: add sign up form

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import SignUpForm from "./components/sign-up-form";
+
+const AuthenticationPage = () => {
+    
+    return (
+        <div className="flex h-screen w-screen items-center justify-center">
+            <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Criar Conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>
+                Faça login para continuar.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+                <div className="space-y-1"> 
+                   <label htmlFor="name">Name</label>
+                   <input id="name" defaultValue= "Pedro Duarte" />
+                </div> 
+
+                <div className="space-y-1"> 
+                   <label htmlFor="username">Username</label>
+                   <input id="username" defaultValue= "@pedroduarte" />
+                </div>    
+            </CardContent>
+            <CardFooter>
+              <Button>Entrar</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="register">
+            <SignUpForm />
+        </TabsContent>
+      </Tabs></div>
+    );
+};
+
+export default AuthenticationPage;
