d8caa2976fb2f619f29ff045a1ab03239aa32883
João Vitor Simonetti <jvsimonetti@outlook.com>
2026-01-15 19:09:20 -0400

    feat: implement session management and redirection in authentication and dashboard pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,21 @@
-export default function DashboardPage() {
-  return (
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+  if (!session?.user) {
+    redirect("/authentication")
+  }
+  return(
     <div>
       <h1>Dashboard</h1>
+      <h1>{session?.user?.name}</h1>
+      <h1>{session?.user?.email}</h1>
     </div>
   );
-}
+};
+
+export default DashboardPage
