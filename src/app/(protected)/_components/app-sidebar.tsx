ccc6cea0f2a6ac94a25add5350edfb91546a0c7e
João Vitor Simonetti <jvsimonetti@outlook.com>
2026-01-19 14:19:30 -0400

    feat: add clinic data to user session and render it on sidebar

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -33,6 +33,7 @@ import {
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 
 // Menu items.
 const items = [
@@ -60,6 +61,8 @@ const items = [
 
 export function AppSidebar() {
   const router = useRouter();
+  const session = authClient.useSession();
+
 
   const handleSignOut = async () => {
     await authClient.signOut({
@@ -108,12 +111,19 @@ export function AppSidebar() {
           <SidebarMenuItem>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <Button>Clínica</Button>
+                <SidebarMenuButton size="lg">
+                  <Avatar>
+                    <AvatarFallback>F</AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <p className="text-sm">{session.data?.user.clinic.name}</p>
+                    <p className="text-sm text-muted-foreground">{session.data?.user.email}</p>
+                  </div>
+                </SidebarMenuButton>               
               </DropdownMenuTrigger>
-
               <DropdownMenuContent>
                 <DropdownMenuItem onClick={handleSignOut}>
-                  <LogOut className="mr-2 h-4 w-4" />
+                  <LogOut />
                   Sair
                 </DropdownMenuItem>
               </DropdownMenuContent>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,9 @@ import { db } from "@/db";
 import { betterAuth } from "better-auth"; 
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from "@/db/schema"
+import { customSession } from "better-auth/plugins"
+import { eq } from "drizzle-orm";
+import { usersToClinicsTable } from "@/db/schema";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, { 
@@ -15,6 +18,27 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
         }, 
     },
+    plugins: [
+      customSession( async ({ user, session }) => {
+        const clinics =  await db. query.usersToClinicsTable.findMany({
+          where: eq(usersToClinicsTable.userId, user.id),
+          with: {
+            clinic: true
+          }
+        });
+        const clinic = clinics[0];
+        return{
+          user: {
+            ...user,
+            clinic: {
+              id: clinic.clinicId,
+              name: clinic.clinic.name
+            }
+          },
+          session,
+        }
+      }),
+    ],
 
   user: {
     modelName: "usersTable",
