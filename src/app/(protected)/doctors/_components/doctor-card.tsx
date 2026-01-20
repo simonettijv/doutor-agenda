01beba0196f5472f3caa13da9f8b96b6cbd93201
João Vitor Simonetti <jvsimonetti@outlook.com>
2026-01-19 21:45:21 -0400

    feat: ensure doctor can  be updated

--- src/app/(protected)/doctors/_components/doctor-card.tsx
+++ src/app/(protected)/doctors/_components/doctor-card.tsx
@@ -11,12 +11,15 @@ import { CalendarIcon, ClockIcon, DollarSignIcon } from "lucide-react"
 import UpsertDoctorForm from "./upsert-doctor-form"
 import { getAvailability } from "../_helpers/availability";
 import { formatCurrencyInCents } from "@/helpers/currency";
+import { useState } from "react";
 
 interface DoctorCardProps{
     doctor: typeof doctorsTable.$inferSelect
 }
 
 const DoctorCard = ({doctor}: DoctorCardProps) => {
+    const [isUpesertDoctorDialogOpen, setIsUpsertDoctorDialogOpen] =
+    useState(false);
     const doctorInitials = doctor.name.split( "" ).map((name) => name[0]).join("");
     const availability = getAvailability(doctor)
     return (
@@ -52,11 +55,17 @@ const DoctorCard = ({doctor}: DoctorCardProps) => {
             </CardContent>
             <Separator />
             <CardFooter>
-                <Dialog>
+                <Dialog open={isUpesertDoctorDialogOpen} onOpenChange={setIsUpsertDoctorDialogOpen}>
                  <DialogTrigger asChild>
                     <Button className="w-full">Ver detalhes</Button>
                  </DialogTrigger>
-                 <UpsertDoctorForm />
+                 <UpsertDoctorForm doctor={{
+                    ...doctor,
+                    availableFromTime: availability.from.format("HH:mm:ss"),
+                    availableToTime: availability.to.format("HH:mm:ss"),
+                 }}
+                 onSuccess={() => setIsUpsertDoctorDialogOpen(false)}
+                 />
                 </Dialog>
             </CardFooter>
         </Card>
