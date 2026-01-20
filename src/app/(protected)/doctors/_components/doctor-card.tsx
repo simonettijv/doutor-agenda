c02badab3c010d67722bc7fc9679f52db1939668
João Vitor Simonetti <jvsimonetti@outlook.com>
2026-01-19 21:30:27 -0400

    feat: ensure doctors are listed on doctors page

---
+++ src/app/(protected)/doctors/_components/doctor-card.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import { Dialog, DialogTrigger } from "@/components/ui/dialog"
+import { Separator } from "@/components/ui/separator"
+import { doctorsTable } from "@/db/schema"
+import { CalendarIcon, ClockIcon, DollarSignIcon } from "lucide-react"
+import UpsertDoctorForm from "./upsert-doctor-form"
+import { getAvailability } from "../_helpers/availability";
+import { formatCurrencyInCents } from "@/helpers/currency";
+
+interface DoctorCardProps{
+    doctor: typeof doctorsTable.$inferSelect
+}
+
+const DoctorCard = ({doctor}: DoctorCardProps) => {
+    const doctorInitials = doctor.name.split( "" ).map((name) => name[0]).join("");
+    const availability = getAvailability(doctor)
+    return (
+        <Card>
+            <CardHeader>
+                <div className="flex itens-center gap-2">
+                    <Avatar className="h-10 w-10">
+                        <AvatarFallback>{doctorInitials}</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <h3 className="text-sm font-medium">{doctor.name}</h3>
+                        <p className="text-sm text-muted-foreground">{doctor.specialty}</p>
+                    </div>
+                </div>
+            </CardHeader>
+            <Separator />
+            <CardContent className="flex flex-col gap-2">
+                <Badge variant='outline'>
+                    <CalendarIcon className="mr-1"/>
+                    {availability.from.format("dddd")} a {availability.to.format("dddd")}
+                </Badge>
+
+                <Badge variant='outline'>
+                    <ClockIcon className="mr-1"/>
+                    {availability.from.format("HH:mm")} às{" "}
+                    {availability.to.format("HH:mm")}
+                </Badge>
+
+                <Badge variant='outline'>
+                    <DollarSignIcon className="mr-1"/>
+                    {formatCurrencyInCents(doctor.appointmentPriceInCents)}
+                </Badge>
+            </CardContent>
+            <Separator />
+            <CardFooter>
+                <Dialog>
+                 <DialogTrigger asChild>
+                    <Button className="w-full">Ver detalhes</Button>
+                 </DialogTrigger>
+                 <UpsertDoctorForm />
+                </Dialog>
+            </CardFooter>
+        </Card>
+    )
+}
+
+export default DoctorCard
--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -3,6 +3,10 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import AddDoctorButton from "./_components/add-doctor-button";
+import { db } from "@/db";
+import { eq } from "drizzle-orm";
+import { doctorsTable } from "@/db/schema";
+import DoctorCard from "./_components/doctor-card";
 
 const DoctorsPage = async () => {
     const session = await auth.api.getSession({
@@ -11,10 +15,12 @@ const DoctorsPage = async () => {
     if (!session?.user){
         redirect("/authentication")
     }
-    console.log(session.user.clinic)
     if (!session.user.clinic) {
         redirect("/clinic-form")
     }
+    const doctors = await db.query.doctorsTable.findMany({
+        where: eq(doctorsTable.clinicId, session.user.clinic.id)
+    })
     return <PageContainer>
         <PageHeader>
             <PageHeaderContent>
@@ -28,7 +34,9 @@ const DoctorsPage = async () => {
         </PageActions>  
         </PageHeader>
         <PageContent>
-            <h1>Médicos</h1>
+            <div className="grid grid-cols-3 gap-6">
+                {doctors.map(doctor => <DoctorCard key={doctor.id} doctor={doctor} />)}
+            </div>
         </PageContent>
     </PageContainer>
 };
