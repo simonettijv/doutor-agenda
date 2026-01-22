0b8f2faebd70abe09331216f88baf293d30c5a74
João Vitor Simonetti <jvsimonetti@outlook.com>
2026-01-22 11:30:34 -0400

    alguns erros corrigidos

--- src/actions/create-clinic/index.ts
+++ src/actions/create-clinic/index.ts
@@ -1,39 +1,23 @@
 "use server";
 
-import { eq } from "drizzle-orm";
-import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
-import { z } from "zod";
+import { redirect } from "next/navigation";
 
 import { db } from "@/db";
-import { appointmentsTable } from "@/db/schema";
+import { clinicsTable, usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
-import { actionClient } from "@/lib/next-safe-actions";
 
-export const deleteAppointment = actionClient
-  .schema(
-    z.object({
-      id: z.string().uuid(),
-    }),
-  )
-  .action(async ({ parsedInput }) => {
-    const session = await auth.api.getSession({
-      headers: await headers(),
-    });
-    if (!session?.user) {
-      throw new Error("Unauthorized");
-    }
-    const appointment = await db.query.appointmentsTable.findFirst({
-      where: eq(appointmentsTable.id, parsedInput.id),
-    });
-    if (!appointment) {
-      throw new Error("Agendamento não encontrado");
-    }
-    if (appointment.clinicId !== session.user.clinic?.id) {
-      throw new Error("Agendamento não encontrado");
-    }
-    await db
-      .delete(appointmentsTable)
-      .where(eq(appointmentsTable.id, parsedInput.id));
-    revalidatePath("/appointments");
+export const createClinic = async (name: string) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
   });
+  if (!session?.user) {
+    throw new Error("Unauthorized");
+  }
+  const [clinic] = await db.insert(clinicsTable).values({ name }).returning();
+  await db.insert(usersToClinicsTable).values({
+    userId: session.user.id,
+    clinicId: clinic.id,
+  });
+  redirect("/dashboard");
+};
